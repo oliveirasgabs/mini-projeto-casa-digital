9d89322e3007898dc2d78cf11c0c39caebdcc44e
Gabrielly <oliveiragabrielly.dev@gmail.com>
2025-10-14 00:12:13 -0300

    feat: :tada: Cria tabela alunos
    SQL para criação da primeira tabela do projeto, a tabela de alunos.

---
+++ sql/criando_tabelas.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Alunos {
+    aluno_id SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    data_nascimento DATE NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+};
